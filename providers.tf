09a31dc0d724dfff17df40f3dabc132246d2bcad
Richard Cove <39362230+rcove@users.noreply.github.com>
2019-10-21 14:51:44 +1100

    update

--- providers.tf
+++ providers.tf
@@ -1,5 +1,11 @@
 # Configure the Azure Provider
-variable azure_subscription_id {
+
+provider "azurerm" { 
+  subscription_id = "${var.azure_subscription_id}"
+  client_id       = "${var.azure_client_id}"
+  client_secret   = "${var.azure_client_secret}"
+  tenant_id       = "${var.azure_tenant_id}"
+}variable azure_subscription_id {
      type = "string"
 }
  
@@ -13,11 +19,4 @@ variable azure_client_secret {
  
 variable azure_tenant_id {
      type = "string"
-}
-
-provider "azurerm" { 
-  subscription_id = "${var.azure_subscription_id}"
-  client_id       = "${var.azure_client_id}"
-  client_secret   = "${var.azure_client_secret}"
-  tenant_id       = "${var.azure_tenant_id}"
 }
